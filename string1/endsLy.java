6c3a5d16b5f11ca86e98fc2646f73053517c397b
Aala Altubi <aala.altubi@gmail.com>
2025-07-30 16:38:15 +0400

    Add solution for String1

---
+++ string1/endsLy.java
@@ -0,0 +1,9 @@
+public boolean endsLy(String str) {
+  if(str.length()<2)
+  return false;
+  else if(str.substring(str.length()-2,str.length()).equals("ly"))
+  return true;
+  else
+  return false;
+  
+}
---
+++ string1/helloName.java
@@ -0,0 +1,5 @@
+public String helloName(String name) {
+  String greet="Hello ";
+  String exclamation="!";
+  return greet+name+exclamation ;
+}
